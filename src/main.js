554c5c68f896bf27f4c7b111424b2f724eeb9a5a
Akeem Mudashiru <akeemmudash@gmail.com>
2025-03-01 03:42:18 +0100

    update:removed excess pictures

--- src/main.js
+++ src/main.js
@@ -27,11 +27,3 @@ function toggleNavMenu() {
 const images = document.querySelectorAll(".about__image");
 let currentIndex = 0;
 
-function showNextImage() {
-  images.forEach((img) => img.classList.remove("fade"));
-  images[currentIndex].classList.add("fade");
-  currentIndex = (currentIndex + 1) % images.length;
-}
-
-showNextImage();
-setInterval(showNextImage, 4000);
